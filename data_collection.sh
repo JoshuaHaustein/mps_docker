dc5b86dceca4b9a31f99e6d35450de48d9112a7f
Joshua Haustein <haustein@kth.se>
2019-06-19 16:08:32 +0200

    data collectino script. moved box2d_sim_env, manipulation_planning_suite, planner_tests to branch docker

--- data_collection.sh
+++ data_collection.sh
@@ -7,15 +7,20 @@ policyLoc="$HOME/Code/sorting-policy/models"
 
 mark="kth"
 policyName="rand"
+classes=2
+objects=15
 mkdir data_gen
 
-object=215
+# generate worlds
+python3 src/planner_tests/python/scripts/sorting/yumi_generator.py --useFor ${dataFolder} --cls ${classes} \
+	--obj ${objects} --num ${max} --output_path src/planner_tests/data/
+
 for i in `seq 1 $max`
 do
     rosrun planner_tests box2d_alpha_sort --verbose --no-gui --pathInfo --num_iterations $num_iter \
-	--problem src/planner_tests/data/${dataFolder}/${object}objs/$i.yaml \
+	--problem src/planner_tests/data/${dataFolder}/${classes}${objects}objs/$i.yaml \
 	--infoFactor 3.0 \
-	--infoDoc data_gen/${mark}-sortingData/${mark}-${policyName}-${object}/$i \
+	--infoDoc data_gen/${mark}-sortingData/${mark}-${policyName}-${classes}${objects}/$i \
     --policy $policyLoc
     # --saveAllInfo
 done
